ab7ef8d8aa7dd9aa0b33af80e7fde19ae05006f5
xxd384905-crypto <xxd384905@gmail.com>
2026-03-24 04:11:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "discord-human-65wpm-safe",
+  "version": "1.0.0",
+  "description": "Hata korumalı ve 65 WPM hızlı sıralı mesaj botu",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
